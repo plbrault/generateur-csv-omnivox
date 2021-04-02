e18fbafdce17b21cd5ecd567e04f0187cccb0144
Pier-Luc Brault <pier-luc@brault.me>
2021-04-02 19:26:10 -0400

    Parse data from Excel files

---
+++ extract-marks.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+
+import shell from 'shelljs';
+
+import getEvaluationFileList from './lib/getEvaluationFileList.js';
+import extractMarksFromFiles from './lib/extractMarksFromFiles.js';
+
+const [,, directoryName] = process.argv;
+
+if (!directoryName) {
+  console.error('ERREUR: Répertoire non spécifié (Syntaxe: `extraire-notes.js nomDuRepertoire`)');
+  process.exit();
+}
+if (!shell.test('-d', directoryName)) {
+  console.error(`ERREUR: "${directoryName}" n'est pas un répertoire.`);
+  process.exit();
+}
+
+const evaluationFileList = getEvaluationFileList(directoryName);
+
+console.log(`Found ${evaluationFileList.length} evaluation files:`);
+console.log(evaluationFileList);
+
+extractMarksFromFiles(evaluationFileList);
--- extraire-notes.js
+++
@@ -1,2 +0,0 @@
-import xlsx from 'node-xlsx';
-
